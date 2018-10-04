8ea4ae61a18cb69098ca5ed8cc8e0edbd1068641
Robert Biggs <rbiggs@ymail.com>
2018-10-03 20:00:00 -0700

    Major refactor for deep comparison.
    1. Comparison is by value. This is so event for arrays and objects. If they have the same values, they are identical.
    2. Does deep comparison of objects and arrays.
    3. For arrays with the same values, if their order differs, they are not equal: [1,2,3,4] !== [1,3,2,4].
    4. For objects, even if their properties are in different orders but equal, they are equal: {name: 'Joe', job: 'mechanic'} === {job: 'mechanic', name: 'Joe').
    }.
    5. Objects with functions as properties are also compared.
    6. String('text') === 'text'.
    7. Number(123) === 123.
    8. With truthy, falsy values
    9. Functions with the same call signature and properties are not equal if their names are different.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 /**
- * A function to test whether the data provided for updating a component creates a new virtual node or not.
+ * Compare two values to see if they are equal. It does comparison by value.
+ * It treats equality of truthy/falsy values strictly.
+ * undefined != null != false, etc.
+ * Functions doing the same thing but with different names are not equal--their names are different.
+ * NaN and 0/0 are equal.
+ * Number(123) === 123
+ * String('text') === 'text'
+ * Arrays with the same values but in different orders are not equal:
+ * [1,2,3,4] !== [1,3,2,4].
+ * Objects with the same properties in different orders are equal.
+ * An object with a static property with value equal to object that inherits its property from another object are not equal (prototype inheritance makes them different).
  * @param {any} value1 A value to check.
  * @param {any} value2 Another value to check.
  * @return {boolean} boolean
  */
 export function areEqual(value1, value2) {
-  return JSON.stringify(value1) === JSON.stringify(value2)
+  // Deal with undefined value:
+  if (value1 === undefined && value2 !== undefined) {
+    return false
+  } else if (value2 === undefined && value1 !== undefined) {
+    return false
+  // Values types are different, so reject:
+  } else if (typeof value1 !== typeof value2) {
+    return false
+  // Compare two functions.
+  // They can do same thing but if names are different, 
+  // they are not equal.
+  } else if (typeof value1 === 'function') {
+    return value1.toString() === value2.toString()
+  // We are dealing with simple types 
+  } else if (typeof value1 !== 'object' && typeof value2 !== 'object') {
+    return JSON.stringify(value1) === JSON.stringify(value2)
+  } else {
+    // Comparing two objects:
+    for (var prop in value1) {
+      if (value1.hasOwnProperty(prop)) {
+        if (value2.hasOwnProperty(prop)) {
+          if (typeof value1[prop] === 'object') {
+            if (!areEqual(value1[prop], value2[prop])) return false
+          } else {
+            if (value1[prop] !== value2[prop]) return false
+          }
+        } else {
+          return false
+        }
+      }
+    }
+    return true
+  }
 }
--- test/areEqual.test.js
+++ test/areEqual.test.js
@@ -6,10 +6,10 @@ test("two identical strings should be equal", function () {
   expect(areEqual(str1, str2)).toBe(true)
 })
 
-test("two identical strings should be equal", function () {
+test("two different strings should not be equal", function () {
   const str1 = 'some text'
-  const str2 = 'some text'
-  expect(areEqual(str1, str2)).toBe(true)
+  const str2 = 'different text'
+  expect(areEqual(str1, str2)).toBe(false)
 })
 
 test("two identical numbers should be equal", function () {
@@ -18,10 +18,27 @@ test("two identical numbers should be equal", function () {
   expect(areEqual(num1, num2)).toBe(true)
 })
 
+test("two different numbers should not be equal", function () {
+  const num1 = 123
+  const num2 = 345
+  expect(areEqual(num1, num2)).toBe(false)
+})
+
 test("two undefined variables should be equal", function () {
   let undef1
   let undef2
   expect(areEqual(undef1, undef2)).toBe(true)
+})
+
+test("values with different types should not be equal", function () {
+  expect(areEqual(123, 'a string')).toBe(false)
+})
+
+test("An undefined variable should not be equal to a defined one", function () {
+  let undef
+  let def = 'Something'
+  expect(areEqual(undef, def)).toBe(false)
+  expect(areEqual(def, undef)).toBe(false)
 })
 
 test("two null variables should be equal", function () {
@@ -48,6 +65,24 @@ test("two arrays with same values should be equal", function () {
   expect(areEqual(array1, array2)).toBe(true)
 })
 
+test("two arrays with same reversed values should not be equal", function () {
+  const array1 = [1, 2, 3]
+  const array2 = [3, 2, 1]
+  expect(areEqual(array1, array2)).toBe(false)
+})
+
+test("two arrays with same objects should be equal", function () {
+  const array1 = [{name: 'Joe'}, {name: 'Sarah'}, {name: 'Sam'}]
+  const array2 = [{ name: 'Joe' }, { name: 'Sarah' }, { name: 'Sam' }]
+  expect(areEqual(array1, array2)).toBe(true)
+})
+
+test("two arrays with same objects in different order should not be equal", function () {
+  const array1 = [{ name: 'Joe' }, { name: 'Sarah' }, { name: 'Sam' }]
+  const array2 = [{ name: 'Sarah' }, { name: 'Joe' }, { name: 'Sam' }]
+  expect(areEqual(array1, array2)).toBe(false)
+})
+
 test("two NaN values should be equal", function () {
   const obj1 = { name: 'Joe' }
   const obj2 = { name: 'Joe' }
@@ -65,4 +100,50 @@ test("two objects with same values should be equal", function () {
   const obj1 = { name: 'Joe' }
   const obj2 = { name: 'Joe' }
   expect(areEqual(obj1, obj2)).toBe(true)
+})
+
+
+test("two objects with values reversed should be equal", function () {
+  const obj1 = { name: 'Joe', job: 'mechanic' }
+  const obj2 = { job: 'mechanic', name: 'Joe' }
+  expect(areEqual(obj1, obj2)).toBe(true)
+})
+
+test("two objects with different values should not be equal", function () {
+  const obj1 = { name: 'Joe', job: 'mechanic' }
+  const obj2 = { name: 'Joe', job: 'lawyer' }
+  expect(areEqual(obj1, obj2)).toBe(false)
+})
+
+
+test("two objects with different values should not be equal", function () {
+  const obj1 = { name: 'Joe', stats: { age: 28, height: `5'/10"` } }
+  const obj2 = { name: 'Joe', stats: { age: 32, height: `5'/10"` } }
+  expect(areEqual(obj1, obj2)).toBe(false)
+})
+
+
+test("two identical functions with different names should not be equal. In this case, one object shares value with another.", function () {
+  function Guy1() {
+    this.name = 'Joe'
+  }
+  function Guy2() {
+    this.name = 'Joe'
+  }
+  expect(areEqual(Guy1, Guy2)).toBe(false)
+})
+
+test("Object that inherit property from another should be different from static property", function () {
+  function Guy1() {
+    this.name = 'Sam'
+    return {name: this.name}
+  }
+  function Guy2() {
+    var name = 'Joe'
+    return name
+  }
+  var guy1 = new Guy1()
+  var guy2 = {}
+  guy2.name = Guy2()
+  expect(areEqual(Guy1, Guy2)).toBe(false)
 })
