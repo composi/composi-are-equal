cc748c8fd35ba9b6d4c7553a37c408a629fc4276
Robert Biggs <rbiggs@ymail.com>
2019-11-11 07:36:20 -0800

    Simplified comparison of simple types.

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ export function areEqual(value1, value2) {
     return value1.toString() === value2.toString()
     // We are dealing with simple types
   } else if (typeof value1 !== 'object' && typeof value2 !== 'object') {
-    return JSON.stringify(value1) === JSON.stringify(value2)
+    return String(value1) === String(value2)
   } else {
     // Comparing two objects:
     for (let prop in value1) {
